b19da2a14d9542172776efafb5d14a4d9994ea71
Asher-ZZ <chitminzawchitminzaw04@gmail.com>
2025-08-14 13:36:57 +0630

    enq

--- src/org/ace/accounting/system/fire/service/interfaces/IFireProposalService.java
+++ src/org/ace/accounting/system/fire/service/interfaces/IFireProposalService.java
@@ -1,5 +1,6 @@
 package org.ace.accounting.system.fire.service.interfaces;
 
+import java.util.Date;
 import java.util.List;
 import org.ace.accounting.system.fire.FireProposal;
 import org.ace.java.component.SystemException;
@@ -15,4 +16,10 @@ public interface IFireProposalService {
     List<FireProposal> findAllFireProposals() throws SystemException;
 
     FireProposal findById(String id) throws SystemException;
+    
+
+    FireProposal findFireProposalByPolicyNo(String policyNo);
+    
+    List<FireProposal> findFireProposalsByDateRange(Date startDate, Date endDate);
+
 }
